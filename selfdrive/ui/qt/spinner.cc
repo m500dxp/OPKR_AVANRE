77d9d19ce3054879be88ad61824544b9180044e8
fallen8angel2 <104266965+fallen8angel2@users.noreply.github.com>
2024-07-01 23:29:10 +0900

    Update spinner.cc

--- selfdrive/ui/qt/spinner.cc
+++ selfdrive/ui/qt/spinner.cc
@@ -55,7 +55,7 @@ Spinner::Spinner(QWidget *parent) : QWidget(parent) {
   btElapsed.start();
   QGridLayout *main_layout = new QGridLayout(this);
   main_layout->setSpacing(0);
-  main_layout->setMargin(200);
+  main_layout->setContentsMargins(0, 0, 50, 200);
 
   main_layout->addWidget(new TrackWidget(this), 0, 0, Qt::AlignHCenter | Qt::AlignVCenter);
 
